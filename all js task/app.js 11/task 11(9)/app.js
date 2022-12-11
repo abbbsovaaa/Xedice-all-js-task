b322f00bc37f49ae708036fa261128178cfb46a9
abbbsovaaa <bbsvaaa10@gmail.com>
2022-12-11 13:01:02 +0400

    add js task 11

---
+++ all js task/app.js 11/task 11(9)/app.js
@@ -0,0 +1,9 @@
+// function toCapitalCase(str){
+    // return str[0].toUpperCase()+str.slice(1,str.length)
+// }
+// console.log(toCapitalCase("ali"))
+
+
+let txt = 'Alion '
+const index = txt.indexOf(' ')
+console.log(txt.slice(1, index))
