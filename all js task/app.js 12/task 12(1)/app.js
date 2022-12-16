21cafd3d96ed48e0ab375ef66545358b97f112e5
abbbsovaaa <bbsvaaa10@gmail.com>
2022-12-16 12:28:54 +0400

    add js task 12

---
+++ all js task/app.js 12/task 12(1)/app.js
@@ -0,0 +1,12 @@
+function removeElement(arr, eded) {
+    const ed = []
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== eded) {
+            ed.push(arr[i])
+        }
+    }
+    return ed
+}
+
+
+console.log(removeElement([1, 2, 3, 4], 3))
