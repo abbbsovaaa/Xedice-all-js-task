02c0adab5f214d4130c356d7ac9fe0823b88e216
abbbsovaaa <bbsvaaa10@gmail.com>
2023-01-20 15:20:34 +0400

    add js task 17

---
+++ all js task/app.js 17/app.js
@@ -0,0 +1,18 @@
+const obj = {
+    height: 172,
+    mass: 77,
+    haircolor: 'blond',
+    skin_color: "fair",
+    eye_color: "blue",
+    birth_year: "19BBY",
+    gender: "male",
+    created: "2023-01-19T15:41:00.986Z",
+    edited: "2023-01-19T15:41:00.986Z",
+    name: "Luke Skywalker",
+
+}
+
+
+const json = JSON.stringify(obj)
+
+console.log(json)
